71ea9d3342c4ec10a5a62c550b47428a015f613f
joemphilips <joemphilips@gmail.com>
2018-05-09 17:45:03 +0900

    switch development environment by NODE_ENV (not by ifdef)

--- app/index.ts
+++ app/index.ts
@@ -1,37 +1,37 @@
 // tslint:disable:no-if-statement
-import { setup, run } from '@cycle/run';
-import isolate from '@cycle/isolate';
+import { setup, run } from "@cycle/run";
+import isolate from "@cycle/isolate";
 /// #if DEVELOPMENT
-import { restartable, rerunner } from 'cycle-restart';
+import { restartable, rerunner } from "cycle-restart";
 /// #endif
 
-import { buildDrivers, wrapMain } from './drivers';
-import { App } from './components/app';
+import { buildDrivers, wrapMain } from "./drivers";
+import { App } from "./components/app";
 
 const main = wrapMain(App as any);
 
-/// #if PRODUCTION
-run(main as any, buildDrivers(([k, t]) => [k, t()]));
+if (process.env.NODE_ENV === "production") {
+  run(main as any, buildDrivers(([k, t]) => [k, t()]));
+} else {
+  const mkDrivers = () =>
+    buildDrivers(([k, t]) => {
+      if (k === "DOM") {
+        return [k, restartable(t(), { pauseSinksWhileReplaying: false })];
+      }
+      if (k === "time" || k === "router") {
+        return [k, t()];
+      }
+      return [k, restartable(t())];
+    });
+  const rerun = rerunner(setup, mkDrivers, isolate);
+  rerun(main as any);
 
-/// #else
-const mkDrivers = () =>
-  buildDrivers(([k, t]) => {
-    if (k === 'DOM') {
-      return [k, restartable(t(), { pauseSinksWhileReplaying: false })];
-    }
-    if (k === 'time' || k === 'router') {
-      return [k, t()];
-    }
-    return [k, restartable(t())];
-  });
-const rerun = rerunner(setup, mkDrivers, isolate);
-rerun(main as any);
+  if (module.hot) {
+    module.hot.accept("./components/app", () => {
+      const newApp = (require("./components/app") as any).App;
 
-if (module.hot) {
-  module.hot.accept('./components/app', () => {
-    const newApp = (require('./components/app') as any).App;
-
-    rerun(wrapMain(newApp));
-  });
+      rerun(wrapMain(newApp));
+    });
+  }
 }
 /// #endif
